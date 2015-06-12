5018a16bf3848818915e56af090322abcde09532
ougarcia <oz.ulysses@gmail.com>
2015-06-12 09:41:38 -0700

    add notebook and tags to notes show

--- app/assets/javascripts/models/note.js
+++ app/assets/javascripts/models/note.js
@@ -1,3 +1,20 @@
 cleverNote.Models.Note = Backbone.Model.extend({
-  urlRoot: '/api/notes'
+  urlRoot: '/api/notes',
+
+  tags: function () {
+    this._tags = this._tags || new cleverNote.Collections.Tags({
+      note: this
+    });
+    return this._tags;
+  },
+
+
+  parse: function (response) {
+    if (response.tags) {
+      this.tags().set(response.tags);
+      delete response['tags'];
+    }
+
+    return response;
+  }
 });
