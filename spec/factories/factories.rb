e9b1ea5dc0545b8262d08a63b5193e0359a8faf5
ougarcia <oz.ulysses@gmail.com>
2016-02-24 13:13:41 -0800

    write specs for Notebook

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,10 +1,4 @@
 require 'faker'
 
 FactoryGirl.define do
-  factory :notebook do 
-    title { Faker::Hacker.noun }
-    created_at { 10.days.ago }
-    updated_at { 5.days.ago }
-    user
-  end
 end
