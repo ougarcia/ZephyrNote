7d288a8d521254a86bb0176f3152f5da5be1989a
ougarcia <oz.ulysses@gmail.com>
2016-02-24 13:03:47 -0800

    write specs for Note

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -5,13 +5,7 @@ FactoryGirl.define do
     title { Faker::Hacker.noun }
     created_at { 10.days.ago }
     updated_at { 5.days.ago }
-  end
-
-  factory :note do
-    title { Faker::Lorem.sentence }
-    body { Faker::Hacker.say_something_smart }
-    created_at { 10.minutes.ago }
-    updated_at { 5.minutes.ago }
+    user
   end
 
   factory :tagging do
